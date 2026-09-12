f639d65c538197634c22548830632d5546d352f1
Navneet Pandey <apna-github-email@gmail.com>
2026-09-12 17:40:05 +0530

    Prepare server for deployment

--- server/server.js
+++ server/server.js
@@ -185,12 +185,12 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   await connectDB();
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
